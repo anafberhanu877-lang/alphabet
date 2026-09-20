3bac4c987b55491123449dddfc3a165a89f5a8dd
anafberhanu877-lang <anafberhanu877@gmail.com>
2026-09-20 17:06:16 +0300

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,57 @@
+c = input("Enter your character: ")
+
+if c == "a":
+    print("This character is part of the alphabet")
+elif c == "b":
+    print("This character is part of the alphabet")
+elif c == "c":
+    print("This character is part of the alphabet")
+elif c == "d":
+    print("This character is part of the alphabet")
+elif c == "e":
+    print("This character is part of the alphabet")
+elif c == "f":
+    print("This character is part of the alphabet")
+elif c == "g":
+    print("This character is part of the alphabet")
+elif c == "h":
+    print("This character is part of the alphabet")
+elif c == "i":
+    print("This character is part of the alphabet")
+elif c == "j":
+    print("This character is part of the alphabet")
+elif c == "k":
+    print("This character is part of the alphabet")
+elif c == "l":
+    print("This character is part of the alphabet")
+elif c == "m":
+    print("This character is part of the alphabet")
+elif c == "n":
+    print("This character is part of the alphabet")
+elif c == "o":
+    print("This character is part of the alphabet")
+elif c == "p":
+    print("This character is part of the alphabet")
+elif c == "q":
+    print("This character is part of the alphabet")
+elif c == "r":
+    print("This character is part of the alphabet")
+elif c == "s":
+    print("This character is part of the alphabet")
+elif c == "t":
+    print("This character is part of the alphabet")
+elif c == "u":
+    print("This character is part of the alphabet")
+elif c == "v":
+    print("This character is part of the alphabet")
+elif c == "w":
+    print("This character is part of the alphabet")
+elif c == "x":
+    print("This character is part of the alphabet")
+elif c == "y":
+    print("This character is part of the alphabet")
+elif c == "z":
+    print("This character is part of the alphabet")
+else:
+    print("This character is not part of the alphabet")
+
